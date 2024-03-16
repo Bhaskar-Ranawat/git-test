13fee6e11b47eb972aeb05592a362733ec8c314c
Bhaskar ranawat <bhaskarshekhar6500@gmail.com>
2024-03-16 17:42:32 +0530

    Added obj in file4

---
+++ file4.js
@@ -0,0 +1,14 @@
+const obj = 
+    [
+        {
+            name: "Bhaskar",
+            age: 28,
+        },
+        {
+            name: 'Bhavna',
+            age: 30
+        }
+    ];
+
+const stringObj = JSON.stringify(obj);
+console.log(stringObj);
